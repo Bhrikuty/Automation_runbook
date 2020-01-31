3d695d182089a549039fd27695edbbcb71b7ed47
Bhrikuty Aggarwal <bhrikuty.aggarwal@gmail.com>
2020-01-31 17:00:15 +0530

    Create PublishEvent.ps1

---
+++ scripts/PublishEvent.ps1
@@ -0,0 +1,25 @@
+#code for sending custom data to subscribed endpoint. Need to embed this in azure function and also need to write the handler part in the application
+
+$endpoint = "https://firsttesttopic.eastus2-1.eventgrid.azure.net/api/events"
+$keys = "u4qbf9Eb/zP8zA3eYAMb60V3TY+UDrA8Bt1O3EuJYGM="
+$eventID = Get-Random 99999
+
+#Date format should be SortableDateTimePattern (ISO 8601)
+$eventDate = Get-Date -Format s
+
+#Construct body using Hashtable
+$htbody = @{
+    id= $eventID
+    eventType="recordInserted"
+    subject="AzureOnboardingStatus"
+    eventTime= $eventDate   
+    data= @{
+        Status="Pending"
+    }
+    dataVersion="1.0"
+}
+
+#Use ConvertTo-Json to convert event body from Hashtable to JSON Object
+#Append square brackets to the converted JSON payload since they are expected in the event's JSON payload syntax
+$body = "["+(ConvertTo-Json $htbody)+"]"
+Invoke-WebRequest -Uri $endpoint -Method POST -Body $body -Headers @{"aeg-sas-key" = $keys}

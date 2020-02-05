51d52989d84e8484b84f99edcdefee0615a1db94
Bhrikuty Aggarwal <bhrikuty.aggarwal@gmail.com>
2020-02-05 20:45:34 +0530

    Update hello.ps1

--- scripts/hello.ps1
+++ scripts/hello.ps1
@@ -5,4 +5,15 @@ param (
     [string]$mysub
   
 )
-az ad sp create-for-rbac --role="Owner" --scopes="/subscriptions/$mysub"  
+# Create the application
+
+$SecureStringPassword = ConvertTo-SecureString -String "dkw1sba4syem37jq" -AsPlainText -Force
+$AppID= New-AzureRmADApplication -DisplayName "CustomerApplication" -IdentifierUris "https://google.com" -HomePage "https://google.com"  -Password $SecureStringPassword
+$AID = $AppID.ApplicationId
+#Create a Service Principal for the Application
+
+$SPID = New-AzureRmADServicePrincipal -ApplicationID $AID
+$SID = $SPID.Id
+# Assign the Role to Service Principal:
+
+New-AzureRmRoleAssignment -Scope /subscriptions/$mysub -ObjectId $SID -RoleDefinitionId 8e3af657-a8ff-443c-a75c-2fe8c4bcb635
